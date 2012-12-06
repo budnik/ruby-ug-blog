13d91b77cfdc7ef9209f70e1568b7e86426ddaf6
dmitriybudnik <dmitriy.budnik@gmail.com>
2012-12-06 17:34:37 +0200

    added linebrake

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Blog::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, 
+             :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   devise_scope :user do
     get 'sign_in', :to => 'users_sessions#new', :as => :new_user_session
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
-
 
   resources :posts
   root :to => 'posts#index'
