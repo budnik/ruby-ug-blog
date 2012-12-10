4fb71a60a1ed8a0e811cba42036545a8a33a1f64
Dmitriy Budnik <dmitriy.budnik@gmail.com>
2012-12-10 14:03:15 +0200

    fixed belong_to :class_name syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
   validates_presence_of :body
   validates_presence_of :author_id
   validates :body, :length => { :maximum => 1024 }
-  belongs_to :author, :class_name => :user, :foreign_key => :author_id
+  belongs_to :author, :class_name => "User", :foreign_key => :author_id
 end
